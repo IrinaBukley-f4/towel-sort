39fd074bf6cba5e617d84e3d5b5db20c1cb5824c
Irina <irenserge@mail.ru>
2026-05-06 15:56:49 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function towelSort(/* matrix */) {
-  throw new Error('Not implemented');
+module.exports = function towelSort(matrix) {
+  if (!matrix || !Array.isArray(matrix)) {
+    return [];
+  }
+  const myArr = [];
+  matrix
+    .map((array) =>
+      matrix.indexOf(array) % 2 !== 0
+        ? array.sort((a, b) => b - a)
+        : array.sort((a, b) => a - b)
+    )
+    .map((elem) => {
+      myArr.push(...elem);
+      return myArr;
+    });
+  return myArr;
 };
